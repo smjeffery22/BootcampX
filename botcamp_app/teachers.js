9ed019f55de43fb4ef65069b76395c906226e19b
Jeffery <parkjon8@gmail.com>
2022-01-06 04:43:29 +0000

    Update to use parameterized queries

--- botcamp_app/teachers.js
+++ botcamp_app/teachers.js
@@ -15,9 +15,9 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`, [`${cohortName}`])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
